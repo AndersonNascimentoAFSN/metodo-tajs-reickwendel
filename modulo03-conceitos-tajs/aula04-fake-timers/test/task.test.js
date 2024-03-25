4621a4808314608e5b67b1c2e483f3342e3c1066
Anderson Nascimento <anderson.nascimento@meta.com.br>
2024-03-25 07:56:19 -0300

    feat: fake timers

---
+++ modulo03-conceitos-tajs/aula04-fake-timers/test/task.test.js
@@ -0,0 +1,75 @@
+import { it, describe, expect, beforeEach, afterEach, jest } from '@jest/globals'
+import Task from '../src/task.js'
+
+import { setTimeout } from 'node:timers/promises'
+
+describe('Task Test Suite', () => {
+  let _logMock
+  let _task
+
+  beforeEach(() => {
+    _logMock = jest.spyOn(console, 'log').mockImplementation()
+    _task = new Task()
+  })
+
+// Forma não certa de trabalhar esperando o tempo das funções e não usando mock para executar rapidamente 
+  it.skip('should only run tasks that are due without fake timers (slow)', async () => {
+    // Arrange, Act, Assert
+
+    // Arrange
+    const tasks = [
+      { name: 'Task-Will-Run-In-5-Secs', dueAt: new Date(Date.now() + 5_000), fn: jest.fn() },
+      { name: 'Task-Will-Run-In-10-Secs', dueAt: new Date(Date.now() + 10_000), fn: jest.fn() },
+    ]
+
+    // Act
+    _task.save(tasks.at(0))
+    _task.save(tasks.at(1))
+
+    _task.run(200) // 200ms
+
+    await setTimeout(11e3) // 11_000ms
+
+    expect(tasks.at(0).fn).toHaveBeenCalled()
+    expect(tasks.at(1).fn).toHaveBeenCalled()
+  }, 
+  // configurar o timeout para 15 segundos nesse test
+  15_000)
+
+  it('should only run tasks that are due with fake timers (fast)', async () => {
+    jest.useFakeTimers() // ativa o fake timer
+    // Arrange, Act, Assert
+
+    // Arrange
+    const tasks = [
+      { name: 'Task-Will-Run-In-5-Secs', dueAt: new Date(Date.now() + 5000), fn: jest.fn() },
+      { name: 'Task-Will-Run-In-10-Secs', dueAt: new Date(Date.now() + 10000), fn: jest.fn() },
+    ]
+
+    // Act
+    _task.save(tasks.at(0))
+    _task.save(tasks.at(1))
+
+    _task.run(200) // 200ms
+
+    jest.advanceTimersByTime(4000) // 4s
+
+    // Não devem ser executados ainda!
+    expect(tasks.at(0).fn).not.toHaveBeenCalled()
+    expect(tasks.at(1).fn).not.toHaveBeenCalled()
+
+    jest.advanceTimersByTime(2000) // 2s
+
+    // 4 + 2 = 6, então só a primeira tarefa deve executar
+    expect(tasks.at(0).fn).toHaveBeenCalled()
+    expect(tasks.at(1).fn).not.toHaveBeenCalled()
+
+    jest.advanceTimersByTime(4000) // 4s
+
+    // 4 + 2 + 4 = 10, as duas tarefas devem executar
+    expect(tasks.at(0).fn).toHaveBeenCalled()
+    expect(tasks.at(1).fn).toHaveBeenCalled()
+
+    jest.useRealTimers() // desativa o fake timer
+  })
+})
